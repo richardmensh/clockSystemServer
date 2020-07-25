32c4504a8a4d5889cf7f3cae5c60d4308a8ae0b9
Richard Essemiah <richarde.dtt@gmail.com>
2020-07-25 19:14:38 +0200

    make it better

--- db/connection.js
+++ db/connection.js
@@ -3,11 +3,16 @@ const mongoose = require('mongoose');
 const URi = "mongodb+srv://richard:richard123@clocksystem.cvsrn.mongodb.net/test?retryWrites=true&w=majority";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI || URi, {
-       useUnifiedTopology: true,
-       useNewUrlParser: true
-    });
-   console.log("db connection");
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || URi, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+         });
+        console.log("db connection");
+    } catch (error) {
+        console.log(error);
+    }
+   
 }
 
 module.exports = connectDB;
