102f6f151a8c82996f294b9d2541375a3b5b1e17
Richard Essemiah <richarde.dtt@gmail.com>
2020-07-25 17:28:49 +0200

    make it better

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
+const URi = "mongodb+srv://richard:richard123@clocksystem.cvsrn.mongodb.net/test?retryWrites=true&w=majority";
+
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI || URi, {
        useUnifiedTopology: true,
        useNewUrlParser: true
     });
-   console.log("db connection on heroku");
+   console.log("db connection");
 }
 
 module.exports = connectDB;
